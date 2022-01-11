e6f6be479de28ad2a058fba0a0c3e130b78c7d5b
DanieleFic <reb_ficus22@hotmail.it>
2022-01-11 15:15:59 +0100

    aggiunti i post tramite js

--- main.js
+++ main.js
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+function creazionePost( posts ){
+    document.querySelector(".posts-list").innerHTML +=
+    `<div class="post">
+    <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data" id="container">
+                        <div class="post-meta__author">${posts.author.name}</div>
+                        <div class="post-meta__time">${posts.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">>${posts.content}</div>
+            <div class="post__image">
+                <img src="${posts.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts.likes}</b> persone
+                    </div>
+                </div> 
+            </div> `           
+  }
+
+  for(let i=0; i < posts.length; i++){
+    creazionePost( posts[i] )
+  }
